a1c2c7c222611f7805e42bb8b6e2707b7b8a5816
Leon Krieg <info@madcow.dev>
2023-07-03 14:51:32 -0500

    Update curl url and make dummy script a bit nicer

--- pages/ftp/install.sh
+++ pages/ftp/install.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-echo "Nothing here yet, sorry.\n"
+print_title()
+{
+echo "\
+  _____ _____        _____   _____
+ / ____|  __ \ /\   |  __ \ / ____|
+| (___ | |__) /  \  | |__\ | |
+ \___ \|  ___/ /\ \ |  _  /| |
+ ____) | |  / ____ \| | \ \| |____
+|_____/|_| /_/    \_\_|  \_\\\_____|
+"
+}
+
+main()
+{
+	print_title
+	echo "No install script written yet. Sorry!"
+	exit 1
+}
+
+main
